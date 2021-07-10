3e6586f4997def6386dc4c128cc544487b07b8fd
julia <https://github.com/couniflower/Projeto-GRASP.git>
2021-07-10 19:43:58 -0300

    Initial commit

--- src/atividade06/Main.java
+++ src/atividade06/Main.java
@@ -2,13 +2,19 @@ package atividade06;
 
 public class Main {
     public static void main(String[] args) {
-        Orcamento o1 = new Orcamento.Builder(80).setImposto(ImpostoX.getInstance()).montar();
+        Orcamento o1 = new Orcamento.Builder(80)
+                .setImposto(ImpostoX.getInstance())
+                .montar();
         o1.info();
 
-        Orcamento o2 = new Orcamento.Builder(30).setImposto(ImpostoY.getInstance()).montar();
+        Orcamento o2 = new Orcamento.Builder(30)
+                .setImposto(ImpostoY.getInstance())
+                .montar();
         o2.info();
 
-        Orcamento o3 = new Orcamento.Builder(30).setImposto(ImpostoZ.getInstance()).montar();
+        Orcamento o3 = new Orcamento.Builder(30)
+                .setImposto(ImpostoZ.getInstance())
+                .montar();
         o3.info();
     }
 }
